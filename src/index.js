5ea0e29527b0f35668eb01d559950ef6a535be4a
Kafui Alordo <kalordo7@gmail.com>
2022-03-11 01:41:26 +0000

    Add a myName module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import _ from 'lodash';
+import myName from './myName';
 
 function component() {
     const element = document.createElement('div');
 
     // lodash now imported by this script
-    element.innerHTML = _.join(['Hello', 'Webpack'], '');
+    // element.innerHTML = _.join(['Hello', 'Webpack'], '');
+
+    // using the myName function import
+    element.textContent = myName('Cody');
 
     return element;
 }
